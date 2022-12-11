2a98cdb87112dc59734287285e34b3d87aa8a5fe
hahahaday12 <hahahaday12@naver.com>
2022-12-11 23:56:53 +0900

    <Bucketlist> 컴포넌트 이름 수정

--- client/src/4_TodoPage/index.js
+++ client/src/4_TodoPage/index.js
@@ -1,29 +1,28 @@
 import Layout from "../Layout/layout"
-import TodoListForm from "./todoList/todoListForm";
-import BurketListForm from "./burketList/burketListForm";
+import TodoList from "./todoList/todoList";
+import BuketList from "./bucketList/bucket";
 import styled from "styled-components";
 
 
 const Fourpage = () => {
-
   return (
     <>
       <Layout>
         <ListWrap>
           <TodoListForm/>
-          <BurketListForm/>
+          <BuketList/>
         </ListWrap>
       </Layout>
     </>
   )
-}
-
+};
 export default Fourpage;
 
-
 const ListWrap = styled.div`
-  width: fit-content;
-  height: auto;
+  width: 1300px;
+  height: 700px;
   display: flex;
+  position: relative;
   justify-content: space-between;
+  right: 250px;
 `
